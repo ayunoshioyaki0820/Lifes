d31b075cd287192e37fc6f99688b85ca1d5b7d5e
ayunoshioyaki0820 <ayumukumano0820@gmail.com>
2025-11-15 21:14:18 +0900

    画面変更対応

--- Lifes/WorldBuildingView.cs
+++ Lifes/WorldBuildingView.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Lifes
 {
@@ -17,24 +18,58 @@ namespace Lifes
         ButtonInput button;
         NumberInput numberInput;
         Grid<NumberInput> grid;
+
+        GraphicsDevice device;
+        SpriteFont font;
+        int lastWidth;
+        int lastHeight;
         public WorldBuildingView(GraphicsDevice device, SpriteFont font)
         {
+            this.device = device;
+            this.font = font;
             whitePixel = new Texture2D(device, 1, 1);
             whitePixel.SetData(new[] { Color.White });
+            
+            Layout();
+        }
+
+        private void Layout()
+        {
             var centerX = device.Viewport.Width / 2;
-            rectangle = new Rectangle(centerX /2, 50, centerX, 40);
+            lastWidth = device.Viewport.Width;
+            lastHeight = device.Viewport.Height;
+
+            rectangle = new Rectangle(centerX / 2, 50, centerX, 40);
+
             inputBox = new TextInput(rectangle, "World Name", font);
-            button = new ButtonInput(new Rectangle(centerX / 2, inputBox.Bottom, centerX, 40), "Create", font);
-            numberInput = new NumberInput(new Rectangle(centerX / 2, button.Bottom, centerX, 40), "Size", font);
-            var widthInput = new NumberInput(new Rectangle(centerX / 2, numberInput.Bottom, centerX, 40), "Width", font);
-            var heightInput = new NumberInput(new Rectangle(centerX / 2, widthInput.Bottom, centerX, 40), "Height", font);
-            var inputGrid = new List<NumberInput> { widthInput, heightInput };
-            grid = new Grid<NumberInput>(font, inputGrid, 10);
+            button = new ButtonInput(
+                new Rectangle(centerX / 2, inputBox.Bottom, centerX, 40),
+                "Create", font
+            );
+            numberInput = new NumberInput(
+                new Rectangle(centerX / 2, button.Bottom, centerX, 40),
+                "Size", font
+            );
+
+            var widthInput = new NumberInput(
+                new Rectangle(centerX / 2, numberInput.Bottom, centerX, 40),
+                "Width", font
+            );
+            var heightInput = new NumberInput(
+                new Rectangle(centerX / 2, widthInput.Bottom, centerX, 40),
+                "Height", font
+            );
+
+            grid = new Grid<NumberInput>(font, new List<NumberInput> { widthInput, heightInput }, 10);
         }
 
 
         public void Update(GameTime gameTime, KeyboardState key, MouseState mouse)
         {
+            if (lastWidth != device.Viewport.Width || lastHeight != device.Viewport.Height)
+            {
+                Layout();
+            }
             inputBox.Update(mouse, key, gameTime);
             button.Update(mouse, key, gameTime);
             numberInput.Update(mouse, key, gameTime);
